dec96d1269778e1713f58ddb3a2cdd25bcc626f4
root <root@H45.hbtn.io>
2022-12-21 10:29:37 -0300

    Fix

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,1 +1,10 @@
 #!/usr/bin/node
+
+let checkbox = document.querySelector('input[type=checkbox]');
+checkbox.addEventListener('change', function () {
+    if (this.checked) {
+        checked.querySelector('#checkbox');
+    } else {
+
+    }
+})
